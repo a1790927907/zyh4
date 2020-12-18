c0eb5fb760fb55ca7550200c0f35d18356065b42
a1790927907 <1790927907@qq.com>
2020-12-18 22:29:52 +0800

    change

--- xxt/get_exam_score.py
+++ xxt/get_exam_score.py
@@ -2,7 +2,7 @@ import json
 import re
 import asyncio
 import aiohttp
-#https://stat2-ans.chaoxing.com/exam-stastics/stu-exam-list?clazzid=32311120&courseid=214717081&cpi=142139892&ut=s&page=1&pageSize=16&personId=72885916
+
 
 async def fetch_name(courseid,classid,cpi,headers):
     name_url = 'https://stat2-ans.chaoxing.com/exam-stastics/stu-exam-stastics?courseid=' + str(
@@ -43,7 +43,7 @@ async def fetch_data_info(courseid,classid,cpi,headers,page):
 
 async def _fetch_person_score(courseid,classid,cpi):
     headers = {
-        'Cookie': 'source=""; lv=0; fid=11889; _uid=143186179; uf=b2d2c93beefa90dcdcba185928a24bfffa0214c848e99fd8630ec718d45a257cce7d9a0ef5e224a87abcdd2aa0f5ad19dc436c455fddffc288b83130e7eb47045ac8670337949add95e6357d4b6573c6b027a337608be8d61a2a387d7c92740e4131f8e6153a856fe7fafd565af53bf2; _d=1607692015003; UID=143186179; vc=D739C359B293732AC3ED20C635D096EE; vc2=E7B1CD3AAD469A65CF3FFE67662D92D6; vc3=A5jBNoYn%2BFkqSsKnED00JYj0peYIgnRhBBiUKHR6tDlzj4YQARk6HP12Pl6rS7NFgWSQS1mT4SVymCN29ztQ69MaJ%2BMkmBvQCtIGePGkCO5FpPjV3aZDd2SU6Qf47Ddez3pJ8JL5BlomJGd24yZHKKv3heCFP4dlNiQKmja3ks0%3D523796bf55bbcb9316943cf80366fe07; xxtenc=60b8f8a9b2998d7aae9cc36dbd81f260; DSSTASH_LOG=C_38-UN_13588-US_143186179-T_1607692015005; spaceFid=11889; spaceRoleId=3; JSESSIONID=73AE2A9E3E4CFEDA9E87349C44AA6439.CurriculumService; route=fd27a25a8235928f99e419cf06dbc48d',
+        'Cookie': 'source=""; lv=0; fid=11889; _uid=143186179; uf=b2d2c93beefa90dcdcba185928a24bfffa0214c848e99fd8630ec718d45a257c0f50073571ce618fbc61d03b712a4f66d110c105546a283d88b83130e7eb47045ac8670337949add95e6357d4b6573c6b027a337608be8d6a98d0ab8000e7340960ef9125354e0b3e7fafd565af53bf2; _d=1608301615369; UID=143186179; vc=D739C359B293732AC3ED20C635D096EE; vc2=499CAFD5BD872EC704B1E8E3F404BE3C; vc3=UBWyKxO0VfP2%2Bhj2pTnfxUY%2BSAv2czpWMcEJjVKr8%2FUGPX4wHi1SfNtkuOhQv%2FVXknnSKto6At4DIWFoDIrMXhdD80yqWCXnn1oSfrHF2LlSKj6cN4WDZnsKEpaDKb8LcJH%2FOrt9aKWNJGynt5wZ08bUXefVYqrHJaPktbR7qTA%3D45750f04ea824a2de4201990fe9a3840; xxtenc=60b8f8a9b2998d7aae9cc36dbd81f260; DSSTASH_LOG=C_38-UN_13588-US_143186179-T_1608301615370; spaceFid=11889; spaceRoleId=3; rt=-2; tl=1; k8s=e66d5328add0c052c4f407b42b64742fbb150f7e; jrose=6C99E3BDE9590FEBA436DD5E9017D145.mooc-2179700257-swt1v; route=2fe558bdb0a1aea656e6ca70ad0cad20; thirdRegist=0',
         "User-Agent":"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
     }
     name_task = asyncio.create_task(fetch_name(courseid,classid,cpi,headers))
